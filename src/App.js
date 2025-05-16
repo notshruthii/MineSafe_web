4ad8d665db844882472a0ad44df2b1fafee1c96d
pj444 <159120290+pj444@users.noreply.github.com>
2025-05-16 23:04:34 +0530

    modifylayout

--- src/App.js
+++ src/App.js
@@ -3,28 +3,23 @@ import React from 'react';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
 import StartShift from './pages/StartShift';
 import SafetyTools from './pages/SafetyTools';
 import TaskLogging from './pages/TaskLogging';
 import EndShift from './pages/EndShift';
 import Logout from './pages/Logout';
 
-
 import Test from './test.js';
-
-
-import Navbar1 from './components/Navbar1.js'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar1 from './components/Navbar1';
 import Navbar from './Navbar';
 import Header from './Header';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 const App = () => {
   return (
-    <div>
-    <Navbar1/>
-    <Test/>
-    
-      <Router>
+    <Router>
       <div className="flex min-h-screen bg-gray-100">
         <Navbar />
         <main className="flex-1 bg-white">
@@ -33,15 +28,24 @@ const App = () => {
             <Route path="/attendance" element={<div className="p-6">Attendance Page</div>} />
             <Route path="/profile" element={<div className="p-6">Profile Page</div>} />
             <Route path="/help" element={<div className="p-6">Help Page</div>} />
-            <Route path="/logout" element={<div className="p-6">Logout Page</div>} />
+            <Route path="/logout" element={<Logout />} />
+
+            {/* Pages for shift management */}
+            <Route path="/start-shift" element={<StartShift />} />
+            <Route path="/safety-tools" element={<SafetyTools />} />
+            <Route path="/task-logging" element={<TaskLogging />} />
+            <Route path="/end-shift" element={<EndShift />} />
+
+            {/* Test page (optional) */}
+            <Route path="/test" element={<Test />} />
           </Routes>
         </main>
       </div>
     </Router>
-    </div>
   );
 };
 
 export default App;
+
 
 
