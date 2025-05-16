07176354d922eab2f9208a8f47cf4ceffa3649af
shruthi <shruthigupta1404@gmail.com>
2025-05-16 11:19:57 +0530

    added tailwind dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar1.js'
+
 const App = () => {
   return (
-    <Navbar/>
+    <div>
+      <Navbar/>
+        </div>
   );
 };
 
