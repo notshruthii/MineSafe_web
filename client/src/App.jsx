217f3fa4caee6bba31fc5f817ffbe11211cb0d86
shruthi <shruthigupta1404@gmail.com>
2025-05-22 23:58:50 +0530

    changed imports, added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,7 @@
 // src/App.js
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
 
-import Header from "./Header.jsx";
-import Navbar1 from "./components/Navbar1";
-import LandingPage from "./Landingpage1.jsx";
-import WorkerDash from "./WorkersDashboard.jsx";
-import SafetyGuidelines from "./Safety_guidelines.jsx";
-import Test from "./test";
-import WorkersLogin from "./WorkersLogin.jsx";
-import ManagersLogin from "./ManagersLogin.jsx";
-import ManagerDashboard from "./Managerdashboard.jsx"; // ✅ ADD THIS
-
-=======
 import Header from "./WorkerPages/Header.jsx";
 import Navbar1 from "./components/Navbar1"; 
 import AttendanceApp from "./LandingPage/NavbarComponents/Attendance.jsx";
@@ -22,7 +10,6 @@ import WorkerDash from './WorkerPages/Workersdashboard.jsx';
 import SafetyGuidelines from "./LandingPage/Safety_guidelines.jsx";
 import Test from "./LandingPage/FirstPage.jsx";
 import WorkersLogin from "./WorkerPages/WorkersLogin.jsx";
->>>>>>> a2f33c7441f7a0cec7513f7454703795570f1aa0
 import "./App.css";
 
 const App = () => {
@@ -33,18 +20,7 @@ const App = () => {
     >
       <Navbar1 />
 
-<<<<<<< HEAD
-      <Routes>
-        <Route path="/" element={<Test />} />
-        <Route path="/dashboard" element={<LandingPage />} />
-        <Route path="/safety-guidelines" element={<SafetyGuidelines />} />
-        <Route path="/workers-login" element={<WorkersLogin />} />
-        <Route path="/manager-login" element={<ManagersLogin />} />
-        <Route path="/worker-dashboard" element={<Header />} />
-        <Route path="/workers/*" element={<WorkerDash />} />
-        <Route path="/manager-dashboard" element={<ManagerDashboard />} /> {/* ✅ ADD THIS */}
-      </Routes>
-=======
+
      <Routes>
   <Route path="/" element={<Test />} />
   <Route path="/dashboard" element={<LandingPage />} />
@@ -55,7 +31,7 @@ const App = () => {
   <Route path="/Attendance" element={<AttendanceApp />} />
 </Routes>
 
->>>>>>> a2f33c7441f7a0cec7513f7454703795570f1aa0
+
     </div>
   );
 };
