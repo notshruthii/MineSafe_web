3897dce2a03972b373a31b4854302fa7181f1bcc
shruthi <shruthigupta1404@gmail.com>
2025-07-11 21:58:00 +0530

    case

--- client/src/Workersdashboard.jsx
+++ client/src/Workersdashboard.jsx
@@ -1,37 +1,37 @@
-// src/WorkersDashboard.js
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+  // src/WorkersDashboard.js
+  import React from "react";
+  import { Routes, Route } from "react-router-dom";
 
-import Header from "./Header";
-import WorkersLogin from "./WorkersLogin";
-import WorkerLayout from "./WorkerLayout";
-import ReportAbnormality from "./LandingPage/NavbarComponents/ReportAbnormality";
-import AttendanceApp from "./Attendance";
-import StartShift from "./pages/StartShift";
-import TaskLogging from "./pages/TaskLogging";
-import EndShift from "./pages/EndShift";
-import SafetyTools from "./pages/SafetyTools";
-import Logout from "./pages/Logout";
+  import Header from "./Header";
+  import WorkersLogin from "./WorkersLogin";
+  import WorkerLayout from "./WorkerLayout";
+  import ReportAbnormality from "./LandingPage/NavbarComponents/ReportAbnormality";
+  import AttendanceApp from "./Attendance";
+  import StartShift from "./pages/StartShift";
+  import TaskLogging from "./pages/TaskLogging";
+  import EndShift from "./pages/EndShift";
+  import SafetyTools from "./pages/SafetyTools";
+  import Logout from "./pages/Logout";
 
-const WorkerDash = () => {
-  return (
-    <Routes>
-      {/* Routes WITHOUT sidebar */}
-      <Route path="workers-login" element={<WorkersLogin />} />
-      <Route path="" element={<Header />} /> {/* this catches / */}
+  const WorkerDash = () => {
+    return (
+      <Routes>
+        {/* Routes WITHOUT sidebar */}
+        <Route path="workers-login" element={<WorkersLogin />} />
+        <Route path="" element={<Header />} /> {/* this catches / */}
 
-      {/* Routes WITH sidebar */}
-      <Route element={<WorkerLayout />}>
-        <Route path="start-shift" element={<StartShift />} />
-        <Route path="task-logging" element={<TaskLogging />} />
-        <Route path="end-shift" element={<EndShift />} />
-        <Route path="safety-tools" element={<SafetyTools />} />
-        <Route path="logout" element={<Logout />} />
-        <Route path="report" element={<ReportAbnormality />} />
-        <Route path="attendance" element={<AttendanceApp />} />
-      </Route>
-    </Routes>
-  );
-};
+        {/* Routes WITH sidebar */}
+        <Route element={<WorkerLayout />}>
+          <Route path="start-shift" element={<StartShift />} />
+          <Route path="task-logging" element={<TaskLogging />} />
+          <Route path="end-shift" element={<EndShift />} />
+          <Route path="safety-tools" element={<SafetyTools />} />
+          <Route path="logout" element={<Logout />} />
+          <Route path="report" element={<ReportAbnormality />} />
+          <Route path="attendance" element={<AttendanceApp />} />
+        </Route>
+      </Routes>
+    );
+  };
 
-export default WorkerDash;
+  export default WorkerDash;
